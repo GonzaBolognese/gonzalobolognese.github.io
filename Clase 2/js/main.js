a5ace0f953862b5c0a8962d497f7895dfb7617e6
Gonzalo <gonza.1703@gmail.com>
2021-10-06 19:17:16 -0300

    Arreglo Clase 2

--- Clase 2/js/main.js
+++ Clase 2/js/main.js
@@ -1,9 +1,9 @@
 let datos = [
-    {id: 1, titulo: 'Carne', cantidad: 12, precio: 2500},
-    {id: 2, titulo: 'Jabon', cantidad: 15, precio: 3000}
+    {titulo: 'Carne', cantidad: 12, precio: 2500},
+    {titulo: 'Jabon', cantidad: 15, precio: 3000}
 ]
 
-let vista = []
+let vista = [] 
 let datosPrevios = []
 
 const btnAgregar = document.querySelector('#btnAgregar')
@@ -11,6 +11,7 @@ const inpBusqueda = document.querySelector('#inpBusqueda')
 const btnDeshacer = document.querySelector('#btnDeshacer')
 const inpAgregar = document.querySelector('#inpAgregar')
 const galeria = document.querySelector('#galeria')
+const lnkLimpiarLista = document.querySelector('#lnkLimpiarLista')
 
 const template = ({titulo, cantidad, precio}) => `<div 
 class="item">
@@ -27,23 +28,28 @@ class="item">
 </div>`
 
 
-//FUNCIONES
+//Funciones
 function render(lista = [{titulo: '', cantidad: 0, precio: 0}]) {
     galeria.innerHTML=''
     lista.forEach(item => {
         galeria.innerHTML += template(item)
     })
 }
-//EVENTOS
-//Al hacer click en el boton Agregar item
 
+//Eventos
+// al cargar la pagina
+document.addEventListener('DOMContentLoaded', () => {
+    render(datos)
+})
+
+//Al hacer click en el boton Agregar item
 btnAgregar.addEventListener('click', () => {
     datosPrevios = datos.slice(0)
-    datos.push([
-        titulo= inpAgregar.value,
-        cantidad= 1,
-        precio= 10
-    ])
+    datos.push({
+        titulo: inpAgregar.value,
+        cantidad: 1,
+        precio: 10
+    })
     render(datos)
     inpAgregar.value = ''
 })
@@ -52,25 +58,30 @@ btnAgregar.addEventListener('click', () => {
 btnDeshacer.addEventListener('click', () => {
     datos = datosPrevios
     render(datos)
-
-
 })
-//Al ingresar datos en el campo de Busqueda
-inpBusqueda.addEventListener('input', e => {
-  /*   let vista = datos.filter((val) => {
-        if (val.titulo.includes(e.target.value)) {
+
+//Al ingresar datos en el campo de busqueda
+inpBusqueda.addEventListener('input', e => {    
+    /* let vista = datos.filter((val) => {
+        if(val.titulo.includes(e.target.value)) {
             return true
-        } else{
+        } else {
             return false
         }
     }) */
     let vista = datos.filter(val => val.titulo.includes(e.target.value))
     render(vista)
 })
-//Al hacer click en limpiar lista
-//Al hacer click en guardar lista
-//Al hacer click en cargar lista
-//Al hacer click en un item con la clase borrar
-//deshacer
 
-//OBJETOS
+//Al hacer click en limpiar lista
+lnkLimpiarLista.addEventListener("click", () => {
+    datosPrevios = datos.slice(0)
+    datos = []
+    render(datos)
+})
+
+//Al hacer click en guardar lista
+//Al hacer click en cargarlista
+//Al hacer click en un item con la clase borrar
+
+//Objetos
